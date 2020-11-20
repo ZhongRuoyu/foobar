16ccb80e4c8994a1d2258afab38da4d31940cb89
Ruoyu <zhongruoyu@outlook.com>
2020-11-20 22:27:09 +0800

    Error fixed

--- 2020-11/hash/numUniqueEmails.c
+++ 2020-11/hash/numUniqueEmails.c
@@ -1,47 +1,64 @@
 // LeetCode Problem 929
-// Runtime 8ms (96.77%)
-// Memory Usage 6.8 MB (96.77%)
+// Runtime: 12 ms (67.74%)
+// Memory Usage: 6.9 MB (83.87%)
 
 #include <stdio.h>
+#define MAX 96
 
-unsigned long getHash(char *str) {
+typedef struct {
+    unsigned long local, domain;
+} emailhash;
+
+unsigned long djb2(char *str) {
     unsigned long hash = 5381UL;
-    while (*str != '@' && *str != '+') {
-        if (*str != '.') hash = ((hash << 5) + hash) + *str;
-        str++;
-    }
-    while (*str != '@') str++;
-    while (*str) {
-        hash = ((hash << 5) + hash) + *(str++);
-    }
+    char c;
+    while (c = *str++) hash = ((hash << 5) + hash) + c;  // hash * 33 + c
     return hash;
-}  // djb2 hash (for unique addresses)
+}
 
-int locateUnique(unsigned long *list, unsigned long *cmp, int c) {
+void resolve(char **s, emailhash *t, int n) {
+    char l[MAX], d[MAX], *p;
+    int x;
+    for (int i = 0; i < n; i++) {
+        p = s[i];
+        x = 0;
+        while (*p != '@' && *p != '+') {
+            if (*p != '.') l[x++] = *p;
+            p++;
+        }
+        l[x] = '\0';
+        while (*(p++) != '@') continue;
+        x = 0;
+        while (d[x++] = *(++p)) continue;
+        t[i].local = djb2(l);
+        t[i].domain = djb2(d);
+    }
+}
+
+int locateUnique(emailhash *u, emailhash *cmp, int c) {
     if (!c) return -1;
     for (int i = 0; i < c; i++) {
-        if (list[i] == *cmp) {
+        if ((u[i].local == cmp->local) &&
+            (u[i].domain == cmp->domain)) {
             return i;
         }
     }
     return -1;
 }
 
-int countUnique(unsigned long *x, int c) {
-    for (int i = 1; i < c; i++) {
+int countUnique(emailhash *x, int n) {
+    for (int i = 1; i < n; i++) {
         if ((locateUnique(x, x + i, i)) != -1) {
-            x[i--] = x[--c];
+            x[i--] = x[--n];
         }
     }
-    return c;
+    return n;
 }
 
 int numUniqueEmails(char **words, int wordsSize) {
-    unsigned long hash[wordsSize];
-    for (int i = 0; i < wordsSize; i++) {
-        hash[i] = getHash(words[i]);
-    }
-    return countUnique(hash, wordsSize);
+    emailhash x[wordsSize];
+    resolve(words, x, wordsSize);
+    return countUnique(x, wordsSize);
 }
 
 int main(void) {
