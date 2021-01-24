372867bfe06db00481838d44e4cdc16fa54c0855
Ruoyu <zhongruoyu@outlook.com>
2021-01-24 11:37:08 +0800

    14 characters reduced

--- 2021-01/n-queens/queen.c
+++ 2021-01/n-queens/queen.c
@@ -1,20 +1,17 @@
           q
-    (n,t,r,c,d,e)
-      {if(r-n){
-       int a=(
-       (1<<n)-
-       1)&(~(c
-      |d|e)),b;
-     while(a)b=a
-       &-a,a&=
-        a-1,t
-        =q(n,
-        t,r+1
-        ,c|b,
-        (d|b)
-       >>1,(e|
-       b)<<1);}
-       else++t;
-      return t;}
-    main(){printf(
-"%d",q(8,0,0,0,0,0));}
+     (n,r,c,d,e)
+      {int a=~-
+       (1<<n)&
+      ~(c|d|e),
+    b,t=1;if(r-n)
+       for(--t
+        ;b=a&
+        -a,a;
+        a&=~b
+        )t+=q
+       (n,r+1,
+       c|b,(d|
+     b)>>1,(e|b)
+    <<1);return t
+   ;}main(){printf
+("%d",q(8,0,0,0,0));}
