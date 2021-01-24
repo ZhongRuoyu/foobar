fbbb8b790aaa8d9847df1468142d0c486ad9dd2e
Ruoyu <zhongruoyu@outlook.com>
2021-01-24 16:57:28 +0800

    2 more characters reduced

--- 2021-01/n-queens/queen.c
+++ 2021-01/n-queens/queen.c
@@ -4,14 +4,14 @@
        (1<<n)&
       ~(c|d|e),
     b,t=1;if(r-n)
-       for(--t
-        ;b=a&
-        -a,a;
-        a&=~b
-        )t+=q
-       (n,r+1,
-       c|b,(d|
-     b)>>1,(e|b)
+      for(--t;b
+        =a&-a
+        ;a&=~
+        b)t+=
+        q(n,r
+        +1,c|
+       b,(d|b)
+      >>1,(e|b)
     <<1);return t
    ;}main(){printf
 ("%d",q(8,0,0,0,0));}
