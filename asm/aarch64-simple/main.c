164e0b1d10e76d1c3f982998e76ece261428ac73
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-05-24 17:36:00 +0800

    asm/aarch64-simple: update

--- asm/aarch64-simple/main.c
+++ asm/aarch64-simple/main.c
@@ -6,8 +6,8 @@ extern int Sum(const int *arr, long long size);
 
 int main(int argc, char **argv) {
   char str[] = "hello, world";
-  // int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
   printf("Strlen(str) = %lld\n", Strlen(str));
-  // printf("Sum(arr, size) = %d\n", Sum(arr, sizeof(arr) / sizeof(*arr)));
+  printf("Sum(arr, size) = %d\n", Sum(arr, sizeof(arr) / sizeof(*arr)));
   return 0;
 }
